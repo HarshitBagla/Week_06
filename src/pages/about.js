099f9e3063536c22ba2ef0e0e5790cf0750b766b
HarshitBagla <harshit.bagla@gmail.com>
2018-10-10 20:34:16 -0500

    Changes need to be made

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
 
 class About extends React.Component {
+  onChange = (event) => {
+    this.setState({term: event.target.value});
+  }
+
+
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      term:'',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+
   render() {
     return (
       <div>
         <h2>About</h2>
+      </div>
+      <div><input value={this.state.term} onChange={this.onChange} /></div>
+      <div>
+        <form className="About" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,21 @@
 import React, { Component } from "react";
 
 class Home extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    term: '',
+    items: []
+  };
+}
   render() {
     return ( <div>
         <h2>Home</h2>
-      </div>);
+      </div>
+      <div>
+        <List items={this.state.items} />
+      </div>
+    );
   }
 }
 
